5b7ecf6055bdd2a48c18f8b3b13d21d9ead9d02f
Jason Hatton <jason.a.hatton@gmail.com>
2015-11-10 22:17:35 -0600

    Added GameClient source file with partial implementation.

---
+++ Client/src/edu/lamar/othelo/client/GameClient.java
@@ -0,0 +1,55 @@
+/*
+ * Jason Hatton
+ */
+
+package edu.lamar.othelo.client;
+
+
+
+public class GameClient extends AbstractClient 
+{
+	
+	private final static MAX_ARGS = 5;
+	
+	private String username, password;
+	private String[] serverArgs = new String[MAX_ARGS];
+	
+	//called upon the client logging in
+	public GameClient(String username, String password,
+	String host, int port) throws IOException
+	{
+		super(host, port);
+		openConnection();
+		sendToServer("connect");
+	}
+	
+	public void quit() throws IOException
+	{
+		closeConnection();
+		System.exit(0);
+	}
+	
+	public void handleMessageFromServer(Object msg)
+	{
+		serverArgs = msg.toString().split(" ");
+		switch (serverArgs[0])
+		{
+			case "login":
+			
+				break;
+			case "register":
+				break;
+			case "start":
+				break;
+			case "move":
+				break;
+		}
+		
+		//here's the idea, split the string and use args[0] as the command
+		//and args[1-n] as the arguments.
+		//no big elseif block, no case where "register login" breaks the system
+		//no need for a message interface
+		
+	}
+	
+}
