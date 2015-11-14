c6915d569acafbcf1bb94de4c997da71ec2f18f9
Jason Hatton <jason.a.hatton@gmail.com>
2015-11-14 13:43:58 -0600

    Changed GUI behavior to terminate on close. Fixed a bug involving infinite recursion on Chessboard class constructor.

--- Client/src/edu/lamar/othelo/client/GUI.java
+++ Client/src/edu/lamar/othelo/client/GUI.java
@@ -1,34 +1,14 @@
 package edu.lamar.othelo.client;
 
-import javax.swing.JPanel;
-
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Graphics;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
-import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JFrame;
-import javax.swing.UIManager;
-import javax.swing.SwingUtilities;
-
-import edu.lamar.othelo.client.Chessboard;
+import javax.swing.*;
 
 public class GUI {
     SpaceState[][] board = new SpaceState[8][8];
-    
-    GUI ()
-    {
+
+    GUI() {
         initializeGUI();
+        drawBoard(board);
+        setSpace(SpaceState.white, 1, 1);
         drawBoard(board);
     }
 
@@ -43,12 +23,12 @@ public class GUI {
     public static void drawBoard(SpaceState[][] gameBoard) {
         Chessboard blah = new Chessboard(gameBoard);
     }
-    
-    public final void initializeGUI()
-    {//Set all cells to empty
+
+    public final void initializeGUI() {//Set all cells to empty
         for (int x = 0; x < 8; x++) {
-             for (int y = 0; y < 8; y++) {
-        board[x][y] = SpaceState.empty; }
+            for (int y = 0; y < 8; y++) {
+                board[x][y] = SpaceState.empty;
+            }
         }
         //Set the 4 starting stones
         board[4][3] = SpaceState.black;
@@ -56,16 +36,15 @@ public class GUI {
         board[3][3] = SpaceState.white;
         board[4][4] = SpaceState.white;
     }
-    
-    public void setSpace(SpaceState state, int row, int column)
-    {
+
+    public void setSpace(SpaceState state, int row, int column) {
         board[row][column] = state;
     }
 
     public enum SpaceState {
         empty, black, white
     }
-    
+
 }
 
 
