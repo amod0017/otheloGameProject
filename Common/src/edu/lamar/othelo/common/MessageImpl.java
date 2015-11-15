475d4d9f8b1def48e5cf5ce433d66d74cd010723
amod0017 <amod0017@gmail.com>
2015-11-15 15:51:23 -0600

    Updated a common framework of communication.

--- Common/src/edu/lamar/othelo/common/MessageImpl.java
+++ Common/src/edu/lamar/othelo/common/MessageImpl.java
@@ -14,10 +14,22 @@ public class MessageImpl implements IMessage {
 	private static final long serialVersionUID = 1L;
 	private final String message;
 	private final String messageType;
+	private final String loginId;
+	private final String makeAMoveCoordinates;
 
-	MessageImpl(final String message, final String messageType) {
+	/**
+	 * makeAMoveCoordinates are null if message is not related to makeAMove.
+	 * @param message
+	 * @param messageType
+	 * @param loginId
+	 * @param makeAMoveCoordinates
+	 */
+	MessageImpl(final String message, final String messageType,
+			final String loginId, final String makeAMoveCoordinates) {
 		this.message = message;
 		this.messageType = messageType;
+		this.loginId = loginId;
+		this.makeAMoveCoordinates = makeAMoveCoordinates;
 	}
 
 	@Override
@@ -28,6 +40,16 @@ public class MessageImpl implements IMessage {
 	@Override
 	public String getMessageType() {
 		return messageType;
+	}
+
+	@Override
+	public String getLogin() {
+		return loginId;
+	}
+
+	@Override
+	public String getMakeAMoveCoordinates() {
+		return makeAMoveCoordinates;
 	}
 
 }
--- Common/src/edu/lamar/othelo/common/irp/IMessage.java
+++ Common/src/edu/lamar/othelo/common/irp/IMessage.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 /**
  * This is a message interface, all the message should be wrapped inside this
  * and sent across.
- * 
+ *
  * @author agehlot
  *
  */
@@ -13,4 +13,8 @@ public interface IMessage extends Serializable {
 	String getMessage();
 
 	String getMessageType();
+
+	String getLogin();
+
+	String getMakeAMoveCoordinates();
 }
