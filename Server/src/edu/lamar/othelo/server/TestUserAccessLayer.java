a72c3cf42d27690354245e3b8014898229e3dad4
amod0017 <amod0017@gmail.com>
2015-11-15 22:37:43 -0600

    Commented the test class written by Ali as of now. Will be modifying once time permits

--- Server/src/edu/lamar/othelo/server/TestUserAccessLayer.java
+++ Server/src/edu/lamar/othelo/server/TestUserAccessLayer.java
@@ -4,8 +4,8 @@ public class TestUserAccessLayer {
 
 	public static void main(final String[] args) {
 		// UserAccessLayer.getInstance().addUser(new User("test", "1234"));
-		final User user = UserAccessLayer.getInstance().getUser("test");
-		System.out.println(user);
+		// final User user = UserAccessLayer.getInstance().getUser("test");
+		// System.out.println(user);
 
 	}
 
